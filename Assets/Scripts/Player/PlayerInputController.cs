5e69f637d966480556d500ce61c2b92262c92ba3
Lan Pham <49123155+KLan98@users.noreply.github.com>
2026-03-23 11:26:03 +0100

    test commit

--- Assets/Scripts/Player/PlayerInputController.cs
+++ Assets/Scripts/Player/PlayerInputController.cs
@@ -82,6 +82,8 @@ public class PlayerInputController : MonoBehaviour
             yield return null;
         }
 
+
+
         transform.position = GetCellCenter(playerPosition);
 
         yield return WaitUntilNextMovement(timeBetweenMovement);
