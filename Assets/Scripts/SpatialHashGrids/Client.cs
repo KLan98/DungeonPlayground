cee59e0de79c823d969e298366ba019ae7f13989
Lan Pham <49123155+KLan98@users.noreply.github.com>
2026-05-16 10:33:52 +0200

    Added new struct for client, plan is to remove the need of Client class for performance since the ground tiles don't need to contain too much information.

--- Assets/Scripts/SpatialHashGrids/Client.cs
+++ Assets/Scripts/SpatialHashGrids/Client.cs
@@ -6,49 +6,43 @@ using UnityEngine;
 [System.Serializable]
 public class Client
 {
-    [SerializeField] private string name;
-    public string Name
-    {
-        get { return name; }
-        set { name = value; }   
-    }
-    [SerializeField] private Vector2 position;
+    [SerializeField] private Vector2 position; // should be in both structs?
     public Vector2 Position
     {
         get { return position; }
         set { position = value; }
     }
-    [SerializeField] private Vector2 dimensions;
+    [SerializeField] private Vector2 dimensions; // want to remove this
     public Vector2 Dimensions
     {
         get { return dimensions;}
         set { dimensions = value; }
     }
-    [SerializeField] private Vector2Int[] indices;
+    [SerializeField] private Vector2Int[] indices; // tileclient
     public Vector2Int[] Indices
     {
         get { return indices;}
         set { indices = value; }
     }
-    [SerializeField] private int queryID;
+    [SerializeField] private int queryID; // optimization field, probably go to tileclient
     public int QueryID
     {
         get{ return queryID;}
         set{ queryID = value;}
     }
-    [SerializeField] private int clientID;
+    [SerializeField] private int clientID; // want to remove this
     public int ClientID
     {
         get { return clientID;}
         set { clientID = value;}
     }
     [SerializeField] private GameObject gameObject; // this can be null, for distinction between immovable environments (floor tiles) and movable game objects
-    public GameObject GameObject
+    public GameObject GameObject // ? need further investigation
     {
         get { return this.gameObject; }
         set { this.gameObject = value; }
     }
-    [SerializeField] private byte entityID; // for entities that have stats
+    [SerializeField] private byte entityID; // entityclient
     public byte EntityID
     {
         get { return entityID; }
@@ -56,16 +50,37 @@ public class Client
     }
 
     //---------------DJIKSTRA MAP FIELDS & PROPERTIES----------------
-    [SerializeField] private bool walkableTile; // OBSTACLES ARE NOT WALKABLE, PLAYER IS NOT WALKABLE TILE
+    [SerializeField] private bool walkableTile; // no need
     public bool WalkableTile
     {
         get { return walkableTile;}
         set { walkableTile = value;}
     }
-    [SerializeField] private int distanceToPlayer;
+    [SerializeField] private int distanceToPlayer; // tileclient
     public int DistanceToPlayer
     {
         get { return distanceToPlayer;}
         set { distanceToPlayer = value;}
     }
+}
+
+// do we need a dedicated struct for tile client?
+public struct TileClient
+{
+    public int QueryID; 
+}
+
+public struct EntityClient
+{
+    public GameObject GameObject;
+    public byte EntityID;
+}
+
+// everything in the map is a grid client, they can either be entity client or tile client
+public struct GridClient
+{
+    public Vector2Int[] Bounds; // bottom left and top right corners
+    public Vector2 Position; // position of tile
+    public Vector2 Size;
+    public int DistanceToPlayer;
 }
